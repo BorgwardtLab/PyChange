2dc81a4a914dc6d269d59600b975458ed6745d96
tgumbsch <tgumbsch@gmail.com>
2017-06-08 12:34:57 +0200

    Implementation of five cp detection algorithms

--- PyChange.py
+++ PyChange.py
@@ -6,89 +6,122 @@ Insitute: D-BSSE at ETHZ
 """
 import numpy as np
 import pandas as pd
-import TTest_combinatorical as TC
-import Exact as EX
-import TTest_divide as TD
-import TTest_ANOVA as TA
 import argparse
 
-def solve(seq,method='single_diff'):
-	if method == 'single':
-		cp, p = TD.TTest(seq)
-		cp = [cp]
-		p = p * len(seq)
-	elif method == 'single_diff':
-		cp, p = TD.TTest(diff(seq))
-		cp = [cp]
-		p = p * len(seq)
-	elif method == 'DP':
-		cp = TD.mc_p(seq,0.05/len(seq))
-		p = TC._test_loc(seq,cp,2)
-	elif method == 'DP_diff':
-		cp = TD.mc_p(diff(seq),0.05/len(seq))
-		p = TC._test_loc(seq,cp,2)
-	elif method == 'exact':
-		cp,p = EX.Exact(seq)
-	elif method == 'exact_diff':
-		cp,p = EX.Exact(diff(seq))
-	elif 'exact' in method:
-		no = int(method[-1])
-		if 'diff' in method:
-			seq = diff(seq)
-		cp,p = EX.Exact(seq,no)
-	elif 'ANOVA' in method:
-		if 'diff' in method:
-			seq = diff(seq)
-		cp = TA.Optimal_ANOVA(seq,0.05,2)
-		p = TC._test_loc(seq,cp,2)
-	else:
-		print "Not a known module"
-		cp = [0]
-		p = 1.
 
-	if p > 1:
-		p = 1.
-	return cp, p
+from lib.MaChaMP.MaChaMP import MaChaMP
+from lib.E_Divise.E_Divise import E_Divise
+from lib.PELT.PELT import PELT
+from lib.WBS.WBS import WBS
+from lib.SMUCE.SMUCE import SMUCE
+
+
+def PyChange(seq, transform='none', method='MaChaMP'):
+    tseq = preprocessing(seq, transform)
+    cp = solve(tseq, method)
+    return cp
+
+
+def solve(seq, method):
+    """
+    Apply method to sequence
+    """
+    cp = []
+    if method == 'MaChaMP':
+        cp = MaChaMP(seq)
+    elif method == 'PELT':
+        cp = PELT(seq)
+    elif method == 'WBS':
+        cp = WBS(seq)
+    elif method == 'SMUCE':
+        cp = SMUCE(seq)
+    elif method == 'E-Divise':
+        cp = E_Divise(seq)
+    else:
+        print "Not a known module"
+        cp = [0]
+    return cp
+
+
+def preprocessing(seq, transform):
+    """
+    Preprocessing of time series
+    """
+    if transform == 'diff':
+        seq = diff(seq)
+    elif transform == 'logdiff':
+        seq = logdiff(seq)
+    elif transform == 'percdiff':
+        seq = percdiff(seq)
+    elif transform == 'logpercdiff':
+        seq = logpercdiff(seq)
+    return seq
+
 
 def diff(seq):
-	return [i-j for i,j in zip(seq[1:],seq[:-1])]
+    """
+    Take differences from sequence
+    """
+    return [i - j for i, j in zip(seq[1:], seq[:-1])]
 
 
-def init():
-	d = pd.DataFrame({'A': np.concatenate((np.cumsum(np.random.randn(50)),np.cumsum(np.random.randn(50)+3), np.cumsum(np.random.randn(100)))), 'B': np.concatenate((['C1']*100, ['C2']*100)), 'T': range(200)})
-	d.to_csv('random.csv')
+def logpercdiff(seq):
+    """
+    Take log percentile differences from sequence
+    """
+    return [[np.log((np.abs(i - j) * (i + j) / 2.) + 1.) for i, j in zip(seq[1:], seq[:-1])]]
+
+
+def logdiff(seq):
+    """
+    Take log differences from sequence
+    """
+    return [[np.log(np.abs((i - j)) + 1.) for i, j in zip(seq[1:], seq[:-1])]]
+
+
+def precdiff(seq):
+    """
+    Take percentile diffrerence from sequence
+    """
+    return [[(i - j) * (i + j) / 2. for i, j in zip(seq[1:], seq[:-1])]]
+
+
+def init_random_csv():
+    """
+    Sample csv data set
+    """
+    d = pd.DataFrame({'A': np.concatenate((np.cumsum(np.random.randn(50)), np.cumsum(np.random.randn(50) + 3), np.cumsum(np.random.randn(100)))), 'B': np.concatenate((['C1'] * 100, ['C2'] * 100)), 'T': range(200)})
+    d.to_csv('random.csv')
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = 'Computes changes in time sereis with python with different methods')
-    parser.add_argument('Filename', help='Filename with .csv extension as from QtFy.')
-    parser.add_argument('Cell', help='CellId column name')
-    parser.add_argument('TF', help='Expression level column name')
-    parser.add_argument('Time', help='timepoint column name')
-    parser.add_argument('Type', help='Standard: single_diff. Type of method: single, DP, ANOVA and exact, possible with _diff for differences. For exact also possible exact_diff_3 for three changes on differences')
+    parser = argparse.ArgumentParser(description='Computes changes in time sereis with python with different methods')
+    parser.add_argument('--filename', help='Filename with .csv extension as from QtFy.')
+    parser.add_argument('--cell', help='Cellid column name, separate time series have diffrent cell ids.')
+    parser.add_argument('--values', help='Expression level column name')
+    parser.add_argument('--time', help='timepoint column name')
+    parser.add_argument('--method', choices=['MaChaMP', 'PELT', 'WBS', 'SMUCE', 'E-Divise'], default='MaChaMP', help='Changepoint detection method')
+    parser.add_argument('--preprocessing', choices=['none', 'diff', 'logdiff', 'percdiff', 'logpercdiff'], default='none', help='transformation of time series')
     args = parser.parse_args()
-    name = str(args.Filename)
-    cell = str(args.Cell)
-    TF = str(args.TF)
-    method = str(args.Type)
-    time = str(args.Time)
+    name = str(args.filename)
+    cell = str(args.cell)
+    TF = str(args.values)
+    method = str(args.method)
+    time = str(args.time)
+    transform = str(args.preprocessing)
 
-    #init()
+    # init_random_csv()
 
     data = pd.read_csv(name)
     cells = data[cell].unique()
-    changes = pd.DataFrame({'CellID': [], 'CP': [], 'P': []})
+    changes = pd.DataFrame({'CellID': [], 'CP': []})
 
     for c in cells:
-    	seq = data[data[cell] == c][TF].dropna().values.tolist()
-    	cp, p = solve(seq,method)
-    	changes = pd.concat((changes,pd.DataFrame({'CellID': [c]*len(cp), 'CP': cp, 'P': [p]*len(cp), 'Timepoint': [data[(data[cell]==c) & (data[TF] ==seq[change])][time].values.tolist()[0] for change in cp]})),ignore_index=True)
+        seq = data[data[cell] == c][TF].dropna().values.tolist()
+        cp = PyChange(seq, transform, method)
+        print len(seq), cp
+        changes = pd.concat((changes, pd.DataFrame({'CellID': [c] * len(cp), 'CP': cp, 'Timepoint': [data[(data[cell] == c) & (data[TF] == seq[change])][time].values.tolist()[0] for change in cp]})), ignore_index=True)
 
-    changes.to_csv('Changes'+name)
+    changes.to_csv('Changes' + name)
 
-    #Iterate over all unique Cell ids, get time series and apply Test
+    # Iterate over all unique Cell ids, get time series and apply Test
     # save in changes
-
-
-
-
-
