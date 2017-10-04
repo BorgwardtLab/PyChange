1483b875f72077e3b2e1524687783f61cd3d9596
tgumbsch <tgumbsch@gmail.com>
2017-10-04 17:05:52 +0200

    QChart3o3

--- lib/cpp_kernel/QChart/QChart.cpp
+++ lib/cpp_kernel/QChart/QChart.cpp
@@ -51,7 +51,7 @@ def QChart(seq,maxlike=5. (my analysis maxlike=6.5)):
     */
 
     MCP Return_MCP;
-    double maxlike = 5.;
+    double maxlike = 2.;
     int burn_in_length = 2;
 
     double mean = 0.;
@@ -70,32 +70,33 @@ def QChart(seq,maxlike=5. (my analysis maxlike=6.5)):
     while (i < sequence.size()){
         run_length++;
         if(i>skip){
-            stat = std::sqrt((run_length-1)/run_length) * (sequence[i-1]-mean)/std::sqrt(M2/run_length);
-            std::cout << std::sqrt(float(run_length-1)/run_length)* (sequence[i-1]-mean)/std::sqrt(M2/run_length) << " "; //here is non zero
+            stat = std::sqrt((float)(run_length-1)/run_length) * (sequence[i-1]-mean)/std::sqrt(M2/run_length);
+            //std::cout << i << " " << std::sqrt(float(run_length-1)/run_length)* (sequence[i-1]-mean)/std::sqrt(M2/run_length) << " "; //here is non zero
             boost::math::students_t_distribution<double> distst(run_length-2);
             boost::math::normal_distribution<double> distnorm;
             stat = boost::math::cdf(boost::math::complement(distst, fabs(stat)));
+            //std::cout << stat << " ";
             stat = boost::math::quantile(boost::math::complement(distnorm,fabs(stat)));
-            std::cout << stat << " "; //here is zero
+            //std::cout << stat << " "; //here is zero
 
 
-
-            chart_0 = chart_0+ stat - stat_3;
-            if(stat > 0){
+            // 3 out of 3 test : too many FP
+            chart_0 = stat;
+            if(stat > maxlike){
                 chart_1 = chart_1 +1;
             }
-            if(stat_3>0){
+            if(stat_3> maxlike){
                 chart_1 = chart_1 -1;
             }
-            std::cout << chart_1 << " " << chart_0 << std::endl;
+            //std::cout << chart_1 << " " << chart_0 << std::endl;
 
-            if ((chart_1 < 1 || chart_1 > 4) && (std::abs(chart_0) > maxlike)){
+            if ((chart_1 > 2) && (std::abs(chart_0) > maxlike)){
                 Return_MCP.locations.push_back(i);
                 skip = i+burn_in_length;
 
                 mean = 0;
                 M2 = 0;
-                run_length = 0;
+                run_length = 1;
                 chart_0 = 0;
                 chart_1 = 1;
                 stat = 0;
